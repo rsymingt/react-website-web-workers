40eef4f545c07b85c072f8ef27d861b49f756afd
Ryan <rsymingt@uoguelph.ca>
2019-09-13 18:58:20 -0400

    working with worker threads

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -11,165 +11,11 @@ import { Parallax, Background } from 'react-parallax';
 
 import UserContext from "../UserContext";
 
+import fractalWorker from "./fractal-worker";
+import WebWorker from "./workerSetup";
+
 function useCustomHook() {
 
-}
-
-function centerDistance(x, y) {
-    return Math.round(Math.sqrt(Math.pow(0-x, 2) + Math.pow(0-y, 2)));
-}
-
-// TODO randomize direction?
-function generateBranches( ctx, angleArea, tilt, length, ox, oy, x, y, branches) {
-
-    for(let b = 0; b < branches; b ++) {
-
-        // let newAngle = Math.round(Math.atan2(newY+ox, newX+oy)*180/Math.PI);
-
-        let angle = (2*Math.PI)/branches*b + angleArea/2 + tilt;
-
-        if(ox !== x && oy !== y) {
-            let vx = x - ox;
-            let vy = y - oy;
-
-            let centerAngle = Math.atan2(vy, vx);
-
-            let lowerBound = centerAngle - angleArea/2;
-            let upperBound = centerAngle + angleArea/2;
-
-            let bCoef = angleArea/(branches-1);
-
-            console.log(bCoef);
-
-            angle = bCoef*b + lowerBound;
-        }
-
-        // let angle = (2*Math.PI)/branches*b + angleArea/2 + tilt;
-        let newX = Math.round(Math.cos(angle)*length) + x;
-        let newY = Math.round(Math.sin(angle)*length) + y;
-
-        let angleAreaDegrees = angleArea*180/Math.PI;
-    }
-}
-
-function trippyDrawBranches( ctx, branchArray, width, height, timeDiff ) {
-
-    for(let b in branchArray) {
-        const { x, y, newX, newY } = branchArray[b];
-
-        ctx.moveTo(x, y);
-        ctx.lineTo(timeDiff*newX, timeDiff*newY);
-
-        ctx.clearRect(-width/2, -height/2, width, height);
-        ctx.stroke();
-    }
-
-}
-
-function drawBranches( ctx, branchArray, width, height, timeDiff ) {
-    for(let b in branchArray) {
-        const { x, y, newX, newY } = branchArray[b];
-
-        ctx.moveTo(x, y);
-        ctx.lineTo(timeDiff*(newX - x) + x, timeDiff*(newY - y) + y);
-
-        ctx.clearRect(-width/2, -height/2, width, height);
-        ctx.stroke();
-    }
-}
-
-function animateBranches( ctx, angleArea, length, x, y, branches, branchArray, width, height, depth, time, branchMemoryArray, trippy ) {
-
-    let timeDiff = ((new Date()).getTime() - time.getTime())/1000;
-    // timeDiff=1;
-
-    if(trippy) {
-        trippyDrawBranches(ctx, branchArray, width, height, timeDiff);
-    } else {
-        drawBranches(ctx, branchArray, width, height, timeDiff);
-    }
-
-    // console.log(timeDiff);
-    if(timeDiff >= 1) {
-        setTimeout(() => {
-            let newX = branchArray[0].newX;
-            let newY = branchArray[0].newY;
-
-            drawFractal2(
-                ctx,
-                branchArray,
-                x,
-                y,
-                newX,
-                newY,
-                angleArea,
-                depth+1,
-                0,
-                width,
-                height
-            );
-        }, 1000)
-    } else {
-        window.requestAnimationFrame(() => {
-            animateBranches( ctx, angleArea, length, x, y, branches, branchArray, width, height, depth, time, branchMemoryArray)
-        });
-    }
-}
-
-// angleArea <= 90
-/*
-determine branch direction
-determine branch
-(pseudo) randomize number of branches?
-pseudo randomize direction of branches?
- */
-async function drawFractal2( ctx, branchMemoryArray, ox, oy, x, y, angleArea, depth, branch, width, height ) {
-    if(Math.abs(x) < width/2 && Math.abs(y) < height/2) {
-
-        if(depth > 1) return;
-
-        let branches = (branch === -1) ? Math.round(Math.random()*5 + 3) : Math.round(Math.random()*0 + 3);
-        let length = Math.random()*90 + 10;
-        let branchArray = [];
-
-        generateBranches(ctx, angleArea, 0, length, ox, oy, x, y, branches);
-
-        // for(let b in branchMemoryArray) {
-        //     let branch = branchMemoryArray[b];
-        //     let mx = branch.newX;
-        //     let my = branch.newY;
-        //
-        //     let tilt = Math.atan2(my, mx) - Math.atan2(y, x);
-        //
-        //     generateBranches(ctx, angleArea, tilt, length, ox, oy, mx, my, branches, branchArray, width, height);
-        // }
-
-        window.requestAnimationFrame(() => {
-            animateBranches( ctx, angleArea, length, x, y, branches, branchArray, width, height, depth, new Date(), branchMemoryArray)
-        });
-
-
-    } else {
-
-    }
-}
-
-async function animateFractal( canvas ) {
-    const ctx = canvas.getContext('2d');
-
-    const width = canvas.width;
-    const height = canvas.height;
-
-    ctx.translate(width/2, height/2);
-
-    ctx.beginPath();
-
-    // randomize??
-    ctx.strokeStyle = 'rgba(0, 153, 255, 1)';
-
-    drawFractal2(ctx, [], 0, 0, 0, 0, Math.PI/2, 0, -1, width, height);
-
-    ctx.closePath();
 }
 
 function HomePage() {
@@ -183,11 +29,20 @@ function HomePage() {
     useEffect(() => {
         // Update the document title using the browser API
         document.title = `You clicked ${count} times`;
+        console.log('test');
 
         if(canvasRef !== null) {
             const canvas = canvasRef.current;
-            console.log('start');
-            animateFractal(canvas);
+            const ctx = canvas.getContext('bitmaprenderer');
+
+            const offscreenCanvas = new OffscreenCanvas(canvas.width, canvas.height);
+
+            const worker = new WebWorker(fractalWorker);
+            worker.postMessage({canvas: offscreenCanvas}, [offscreenCanvas]);
+
+            worker.addEventListener('message', (e) => {
+                ctx.transferFromImageBitmap(e.data.bitmap)
+            });
         }
 
         return () => {
@@ -197,7 +52,7 @@ function HomePage() {
 
     return(
         <div>
-            <canvas ref={canvasRef} width={window.innerWidth} height={window.innerWidth}/>
+            <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}/>
             {/*<Parallax*/}
             {/*    blur={10}*/}
             {/*    bgImage={require('path/to/image.jpg')}*/}
